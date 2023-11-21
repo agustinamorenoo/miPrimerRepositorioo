7003ba7effb8ac14ccbad5a4bcb625e487c7092d
agustinamorenoo <150288422+agustinamorenoo@users.noreply.github.com>
2023-11-20 22:20:46 -0300

    Add files via upload

---
+++ challenge7.agusmoreno/radioCirculo/js/radioCirculo.js
@@ -0,0 +1,8 @@
+
+const pi = 3.14
+let radio = parseInt(prompt("Ingrese el radio"))
+let resultado = (pi * radio ** 2)
+document.write("El resultado del area del circulo es igual a:"+" "+ resultado)
+
+
+
---
+++ challenge7.agusmoreno/radioTriangulo/js/radioTriangulo.js
@@ -0,0 +1,4 @@
+let base = parseInt(prompt("Ingrese el base"))
+let altura = parseInt(prompt("Ingrese el altura"))
+let resultado = (base * altura / 2)
+document.write("El resultado del area del triangulo es igual a:"+" "+ resultado)
